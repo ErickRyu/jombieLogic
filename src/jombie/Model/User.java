f8b425195dccc1596bcf098a14cd561244f8c24e
ErickRyu <rkggmdii@gmail.com>
2016-01-29 22:49:42 +0900

    implement comparable, add setUserLocation(int direction) method

--- src/jombie/Model/User.java
+++ src/jombie/Model/User.java
@@ -1,41 +1,60 @@
 package jombie.Model;
 
-public class User {
+public class User implements Comparable<User> {
 	private Location location;
 	private int hp;
 	private String userName;
 	private boolean isJombie;
 	private static final int basicDamage = 20;
 	
-	public User(Location location, String userName){
+	@Override
+	public int compareTo(User user) {
+		if(this.getUserLocation().getLocation_y() == user.getUserLocation().getLocation_y()){
+			return this.getUserLocation().getLocation_x() - user.getUserLocation().getLocation_x();
+		}
+		return this.getUserLocation().getLocation_y() - user.getUserLocation().getLocation_y();
+	}
+
+	public User(Location location, String userName) {
 		this.location = location;
+		this.userName = userName;
 		this.hp = 100;
 		this.isJombie = false;
 	}
-	public Location getUserLocation(){
+
+	public Location getUserLocation() {
 		return location;
 	}
-	public void setUserLocation(){
-		
+	
+	public void setUserLocation(int direction){
+		this.location.moveDirect(direction);
 	}
-	public void attack(User user){
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void attack(User user) {
 		// 자신이 좀비이고 상대가 사람일 경우
-		if(this.isJombie && !user.isJombie){
-			
+		if (this.isJombie && !user.isJombie) {
+
 		}
 	}
-	public void beAttacked(User user){
+
+	public void beAttacked(User user) {
 		setUserHPbyDamaging(basicDamage);
 	}
-	public void setUserHP(int hp){
+
+	public void setUserHP(int hp) {
 		this.hp = hp;
 	}
-	public void setUserHPbyDamaging(int damage){
+
+	public void setUserHPbyDamaging(int damage) {
 		this.hp -= damage;
 	}
-	public int getUserHP(){
+
+	public int getUserHP() {
 		return this.hp;
 	}
-	
 
 }
