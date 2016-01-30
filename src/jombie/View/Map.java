86a73f70e5e50004df64021c8c80878137b6de45
ErickRyu <rkggmdii@gmail.com>
2016-01-30 10:27:24 +0900

    fixed duplicated user problem in printing map method

--- src/jombie/View/Map.java
+++ src/jombie/View/Map.java
@@ -1,49 +1,64 @@
 package jombie.View;
+
 import java.util.ArrayList;
 
 import jombie.Model.User;
+
 public class Map {
 	protected int mapSize_y;
 	protected int mapSize_x;
-	
-	public Map(int y, int x){
+
+	public Map(int y, int x) {
 		mapSize_y = y;
 		mapSize_x = x;
 	}
-	public void drawMap(ArrayList<User> userList){
+
+	public void drawMap(ArrayList<User> userList) {
 		// sparseMatrix 떠올리면 될 것 같음.
-		// 나중에 다시 user는 y, x순으로 정렬해야겠음.
-		int last_y = 0;
-		int last_x = 0;
+		int last_y = -1;
+		int last_x = -1;
 		// 이 방식 문제점 : 유저 좌표가 중복 될 경우 중복해서 계속 출력함.
-		for(User user : userList){
+		String dot;
+		for (User user : userList) {
 			int currentUserPosition_y = user.getUserLocation().getLocation_y();
 			int currentUserPosition_x = user.getUserLocation().getLocation_x();
-			
-			for(; last_y < currentUserPosition_y; last_y++, last_x = 0){
-				
-				for(; last_x < mapSize_x; last_x++){
-					//계속  println 호출하는게 더러움
-					System.out.print(". ");
+			// 중복되는 경우 출력하지 않으려고 했음
+			// 제대로 처리했는지는 잘 모르겠는데, 일단 화면에 중복되지는 않는다.
+			// 문제가 있을거라고 생각했는데, 아니네..
+			if (last_y == currentUserPosition_y && last_x == currentUserPosition_x)
+				continue;
+			else {
+				last_x++;
+			}
+			if (last_y == -1) {
+				last_y = 0;
+			}
+
+			for (; last_y < currentUserPosition_y; last_y++, last_x = 0) {
+				dot = "";
+				for (; last_x < mapSize_x; last_x++) {
+					dot += ". ";
 				}
-				System.out.println();
+				System.out.println(dot);
 			}
-			
-			for(; last_x < currentUserPosition_x; last_x++){
-				System.out.print(". ");
+			dot = "";
+			for (; last_x < currentUserPosition_x; last_x++) {
+				dot += ". ";
 			}
-			System.out.print("* ");
-			++last_x;
+			System.out.print(dot + "* ");
 		}
-		for(; last_y < mapSize_y; last_y++, last_x = 0){
-			for(; last_x < mapSize_x; last_x++){
-				System.out.print(". ");
+		++last_x;
+		for (; last_y < mapSize_y; last_y++, last_x = 0) {
+			dot = "";
+			for (; last_x < mapSize_x; last_x++) {
+				dot += ". ";
 			}
-			System.out.println();			
+			System.out.println(dot);
 		}
 		System.out.println();
 	}
-	public void drawUser(User user){
-		
+
+	public void drawUser(User user) {
+
 	}
 }
